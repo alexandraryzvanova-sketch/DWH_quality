36f7d8353b4dec07490b12aff61faee202a01e88
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-16 12:01:01 +0300

    Update create_dds_tables.sql

--- sql/dds/create_dds_tables.sql
+++ sql/dds/create_dds_tables.sql
@@ -1,1 +1,31 @@
+create schema if not exists dds;
 
+-- Измерение: годы
+create table if not exists dds.dim_year (
+    year_id serial primary key,
+    year int unique
+);
+
+-- Измерение: команды
+create table if not exists dds.dim_team (
+    team_id serial primary key,
+    team_name text unique
+);
+
+-- Измерение: игроки
+create table if not exists dds.dim_player (
+    player_id serial primary key,
+    player_name text unique
+);
+
+-- Измерение: страны
+create table if not exists dds.dim_country (
+    country_id serial primary key,
+    country_name text unique
+);
+
+-- Измерение: герои
+create table if not exists dds.dim_hero (
+    hero_id serial primary key,
+    hero_name text unique
+);
