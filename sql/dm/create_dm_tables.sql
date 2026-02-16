b6a0dbb34497f557e8951287364403a208d0a40e
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-16 13:37:51 +0300

    Update create_dm_tables.sql

--- sql/dm/create_dm_tables.sql
+++ sql/dm/create_dm_tables.sql
@@ -1,1 +1,33 @@
+create schema if not exists dm;
 
+-- Факт победителей
+create table if not exists dm.f_winners_top3 (
+    year_id int not null,
+    team_id int not null,
+    place varchar(10),
+    prize_usd numeric,
+    prize_percent numeric,
+    primary key (year_id, team_id, place)
+);
+
+-- Факт состава команд
+create table if not exists dm.f_team_roster (
+    year_id int not null,
+    team_id int not null,
+    player_id int not null,
+    role text
+);
+
+-- Факт пиков героев
+create table if not exists dm.f_hero_picks (
+    year_id int not null,
+    hero_id int not null,
+    times_picked int
+);
+
+-- Факт игрок-страна
+create table if not exists dm.f_player_country (
+    year_id int not null,
+    player_id int not null,
+    country_id int not null
+);
