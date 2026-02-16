36bc9e8980de2ae2ac7c5263fa28347c64c93459
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-16 11:51:22 +0300

    Update create_stage_tables.sql

--- sql/stage/create_stage_tables.sql
+++ sql/stage/create_stage_tables.sql
@@ -1,1 +1,28 @@
+create schema if not exists stage;
 
+create table if not exists stage.winners_raw (
+    year int,
+    place int,
+    team_name text,
+    prize_usd numeric,
+    prize_percent numeric
+);
+
+create table if not exists stage.teams_raw (
+    player_name text,
+    team_name text,
+    year int,
+    role text
+);
+
+create table if not exists stage.country_raw (
+    player_name text,
+    country text,
+    year int
+);
+
+create table if not exists stage.hero_picks_raw (
+    hero text,
+    times_picked int,
+    year int
+);
