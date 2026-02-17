ead68c78a4f34c8a390b2dd117498a4692c32e3e
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-17 17:22:58 +0300

    Create mart_team_prize.sql

---
+++ sql/marts/mart_team_prize.sql
@@ -0,0 +1,11 @@
+-- Динамика доходов команд
+
+SELECT
+    dy.year,
+    dt.team_name,
+    SUM(f.prize_usd) AS total_prize_usd
+FROM dm.f_winners_top3 f
+JOIN dds.dim_team dt   ON dt.team_id = f.team_id
+JOIN dds.dim_year dy   ON dy.year_id = f.year_id
+GROUP BY dy.year, dt.team_name
+ORDER BY dy.year, total_prize_usd DESC;
