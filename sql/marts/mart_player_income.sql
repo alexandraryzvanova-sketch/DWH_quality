e0e2de0875c212bf0768939f7361b0b883f64397
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-18 20:59:39 +0300

    Update mart_player_income.sql

--- sql/marts/mart_player_income.sql
+++ sql/marts/mart_player_income.sql
@@ -2,12 +2,12 @@
 
 SELECT
     dp.player_name,
-    dc.country,
+    dc.country_name,
     SUM(f.prize_usd / 5.0) AS estimated_income
 FROM dm.f_winners_top3 f
 JOIN dm.f_team_roster r   ON r.team_id = f.team_id AND r.year_id = f.year_id
 JOIN dm.f_player_country pc ON pc.player_id = r.player_id AND pc.year_id = r.year_id
 JOIN dds.dim_player dp ON dp.player_id = r.player_id
 JOIN dds.dim_country dc ON dc.country_id = pc.country_id
-GROUP BY dp.player_name, dc.country
+GROUP BY dp.player_name, dc.country_name
 ORDER BY estimated_income DESC;
