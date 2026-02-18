bd2909d59fb4eff4d300d9637e226f1dff4025c7
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-18 20:57:29 +0300

    Update mart_hero_popularity.sql

--- sql/marts/mart_hero_popularity.sql
+++ sql/marts/mart_hero_popularity.sql
@@ -2,10 +2,10 @@
 
 SELECT
     dy.year,
-    dh.hero,
+    dh.hero_name,
     SUM(f.times_picked) AS picks
 FROM dm.f_hero_picks f
 JOIN dds.dim_hero dh ON dh.hero_id = f.hero_id
 JOIN dds.dim_year dy ON dy.year_id = f.year_id
-GROUP BY dy.year, dh.hero
+GROUP BY dy.year, dh.hero_name
 ORDER BY dy.year, picks DESC;
