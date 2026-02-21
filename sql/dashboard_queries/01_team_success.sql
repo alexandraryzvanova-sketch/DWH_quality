0a81b05964ed3acfa13bd259c6f6b1112223498a
alexandraryzvanova-sketch <alexandraryzvanova@gmail.com>
2026-02-21 14:38:32 +0300

    Update 01_team_success.sql

--- sql/dashboard_queries/01_team_success.sql
+++ sql/dashboard_queries/01_team_success.sql
@@ -1,9 +1,12 @@
--- Команды самые успешные в истории
+-- team_success_combined.sql
+-- Три метрики успешности команд в одном запросе
 SELECT 
     t.team_name,
-    COUNT(*) AS top3_count,
+    COUNT(CASE WHEN w.place = '1' THEN 1 END) AS wins,
+    COUNT(*) AS top3_appearances,
     SUM(w.prize_usd) AS total_prize_usd
 FROM dm.f_winners_top3 w
 JOIN dds.dim_team t ON w.team_id = t.team_id
 GROUP BY t.team_name
-ORDER BY top3_count DESC, total_prize_usd DESC;
+ORDER BY wins DESC, top3_appearances DESC, total_prize_usd DESC;
+
